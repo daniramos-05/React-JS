ded46b9b642b2b8b129f7f3fc13efb037b6168f3
daniramos-05 <dany.ramo.2005@gmail.com>
2025-09-15 23:01:55 -0300

    Vista para celulares

--- react-js/src/components/layouts/NavBar/NavBar.jsx
+++ react-js/src/components/layouts/NavBar/NavBar.jsx
@@ -1,21 +1,24 @@
 import './NavBar.css'
 import CartWidget from '../../common/CartWidget/CartWidget';
 import { Link } from "react-router-dom"
+import { useState } from 'react';
 
-const NavBar = ( {chau, saludo, links, imagen} ) => {
+const NavBar = ({ chau, saludo, links, imagen }) => {
+  const [isOpen, setIsOpen] = useState(false);
 
   return (
     <div className="nav">
       <Link className="mi-tiendita" to="/">
         <h1>Mi Tiendita</h1>
       </Link>
-      <ul className="contenedor-articulos">
-        <Link to="/categoria/remera" className="articulos">Remeras</Link>
-        <Link to="/categoria/campera" className="articulos">Camperas</Link>
-        <Link to="/categoria/pantalon" className="articulos">Pantalones</Link>
-        <Link to="/categoria/calzado" className="articulos">Calzado</Link>
-        <Link to="/categoria/bermuda" className="articulos">Bermudas</Link>
-        <Link to="/categoria/accesorios" className="articulos">Accesorios</Link>
+      <div className="icono" onClick={() => setIsOpen(!isOpen)}>☰</div>
+      <ul className={`contenedor-articulos ${isOpen ? "open" : ""}`}>
+        <li><Link to="/categoria/remera" className="articulos">Remeras</Link></li>
+        <li><Link to="/categoria/campera" className="articulos">Camperas</Link></li>
+        <li><Link to="/categoria/pantalon" className="articulos">Pantalones</Link></li>
+        <li><Link to="/categoria/calzado" className="articulos">Calzado</Link></li>
+        <li><Link to="/categoria/bermuda" className="articulos">Bermudas</Link></li>
+        <li><Link to="/categoria/accesorios" className="articulos">Accesorios</Link></li>
       </ul>
       <CartWidget />
     </div>
